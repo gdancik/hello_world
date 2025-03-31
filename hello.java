e9533616dc246b79a836a6d375a03720a5b060ae
garevalo031308 <g.arevalo031308@gmail.com>
2025-03-31 18:21:51 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,10 +5,7 @@ public class hello {
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
-
-       // delete this line
-       System.out.println("Modify this code");
+       String firstName = "Gabriel";
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
 
