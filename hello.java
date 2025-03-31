4d9d2d60fd777d7a00dc954610d00b35e7903c9f
Talal Shreidi <164095977+talalshreidi@users.noreply.github.com>
2025-03-31 19:59:07 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,12 +5,12 @@ public class hello {
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
+       String firstName = "Talal";
 
-       // delete this line
-       System.out.println("Modify this code");
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
+
+       System.out.println("Hello, I am " + firstName + ".");
 
     }
 }
